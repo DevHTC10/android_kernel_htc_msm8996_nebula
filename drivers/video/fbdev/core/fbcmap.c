a5a768da695707e97d763ed951c6a26c4cf76c0f
Dorimanx <yuri@bynet.co.il>
2017-04-19 18:31:28 +0300

    patch-3.18.48-49 only missing changes.
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/video/fbdev/core/fbcmap.c
+++ drivers/video/fbdev/core/fbcmap.c
@@ -166,6 +166,9 @@ int fb_copy_cmap(const struct fb_cmap *from, struct fb_cmap *to)
 	unsigned int tooff = 0, fromoff = 0;
 	size_t size;
 
+	if ((int)(to->start) < 0)
+		return -EINVAL;
+
 	if (to->start > from->start)
 		fromoff = to->start - from->start;
 	else
@@ -191,6 +194,9 @@ int fb_cmap_to_user(const struct fb_cmap *from, struct fb_cmap_user *to)
 	unsigned int tooff = 0, fromoff = 0;
 	size_t size;
 
+	if ((int)(to->start) < 0)
+		return -EINVAL;
+
 	if (to->start > from->start)
 		fromoff = to->start - from->start;
 	else
@@ -198,12 +204,13 @@ int fb_cmap_to_user(const struct fb_cmap *from, struct fb_cmap_user *to)
 	if (fromoff >= from->len || tooff >= to->len)
 		return -EINVAL;
 
+	if (fromoff >= from->len || tooff >= to->len)
+		return -EINVAL;
+
 	size = min_t(size_t, to->len - tooff, from->len - fromoff);
 	if (size == 0)
 		return -EINVAL;
 	size *= sizeof(u16);
-	if (size == 0)
-		return -EINVAL;
 
 	if (copy_to_user(to->red+tooff, from->red+fromoff, size))
 		return -EFAULT;
