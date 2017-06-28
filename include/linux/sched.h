8df447a2ed3565fe946d4bbed881ea49a3b6bad9
Pavankumar Kondeti <pkondeti@codeaurora.org>
2017-06-28 12:00:31 +0530

    softirq: defer softirq processing to ksoftirqd if CPU is busy with RT
    Defer the softirq processing to ksoftirqd if a RT task is running
    or queued on the current CPU. This complements the RT task placement
    algorithm which tries to find a CPU that is not currently busy with
    softirqs.
    
    Currently NET_TX, NET_RX, BLOCK and TASKLET softirqs are only deferred
    as they can potentially run for long time.
    
    Change-Id: Id7665244af6bbd5a96d9e591cf26154e9eaa860c
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Alex Naidis <alex.naidis@paranoidandroid.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2302,8 +2302,11 @@ extern void do_set_cpus_allowed(struct task_struct *p,
 
 extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
+
 extern void sched_set_cpu_cstate(int cpu, int cstate,
 			 int wakeup_energy, int wakeup_latency);
+
+extern bool cpupri_check_rt(void);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p,
 				      const struct cpumask *new_mask)
