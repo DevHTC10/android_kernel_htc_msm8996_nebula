ed4295304a6c7ca3f8ffda50d71748738ac1321f
Eliminater74 <eliminater74@gmail.com>
2017-09-01 22:26:09 -0400

    [GCC] Fix Compiler Warnings and Bugs, Original By @Dorimanx:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -1687,7 +1687,8 @@ static void functionfs_unbind(struct ffs_data *ffs)
 static int ffs_epfiles_create(struct ffs_data *ffs)
 {
 	struct ffs_epfile *epfile, *epfiles;
-	unsigned i, count;
+	short i;
+	unsigned count;
 
 	ENTER();
 
